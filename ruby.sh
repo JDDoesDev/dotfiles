0a9d42639deada24f90d3198aa15812bb8c8caf4
JD Flynn <jflynn8@gmail.com>
2018-07-11 14:53:35 -0500

    fixing stuff

--- ruby.sh
+++ ruby.sh
@@ -7,7 +7,7 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install RVM stable with ruby and rails
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --rails
 
 echo "source ~/.profile" >> ~/.bash_profile
